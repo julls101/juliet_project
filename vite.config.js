ea09ec736929c38078e764f733d6f1d30bd961d4
julls101 <Jullsull20@gmail.com>
2024-08-08 16:28:44 +0100

    remove some doc

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'src/main.jsx',
-        app: 'src/App.jsx',
-        home: 'src/pages/Home.jsx',
-        product: 'src/product/product.jsx',
-        login: 'src/component/login/login.jsx',
-        signup: 'src/component/signup/signup.jsx',
-        singleView: 'src/component/single-product-view/singleView.jsx',
-        admin: 'src/admin-section/admin.jsx'
-      }
-    }
-  }
+  plugins: [react()]
 })
